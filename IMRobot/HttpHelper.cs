87f25919badecc51060122fd31d35eed940513ae
threecolors <threecolors@126.com>
2015-12-10 18:10:59 +0800

    验证

---
+++ IMRobot/HttpHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMRobot
+{
+    public class HttpHelper
+    {
+        public HttpClient client = new HttpClient();
+
+        public string GetHttpResponseString(string requestUri)
+        {
+            return client.GetStringAsync(requestUri).Result;
+        }
+
+        public Image GetHttpResponseImage(string requestUri)
+        {
+            return Image.FromStream(client.GetStreamAsync(requestUri).Result);
+        }
+    }
+}

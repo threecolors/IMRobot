ee43892fd162845b2e1254a44972c2be02522747
threecolors <threecolors@126.com>
2015-12-15 17:32:13 +0800

    登录完成

--- IMRobot/QQHelper.cs
+++ IMRobot/QQHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -17,13 +18,13 @@ namespace IMRobot
 
         private string verifysession = string.Empty;
 
-        private string qquin = string.Empty;
-
         private string codeString = string.Empty;
+
+        public string logSuccessUrl = string.Empty;
 
         public QQHelper()
         {
-            webBrowser.Navigate(System.IO.Path.GetFullPath("abc.html"));
+            webBrowser.Navigate(System.IO.Path.GetFullPath("webqq.html"));
             webBrowser.ScriptErrorsSuppressed = true;
         }
 
@@ -51,7 +52,6 @@ namespace IMRobot
                     case "0":
                         ////不需要验证码
                         this.verifysession = mc[3].Value;
-                        this.qquin = mc[2].Value;
                         this.codeString = mc[1].Value;
                         break;
                     case "1":
@@ -60,7 +60,6 @@ namespace IMRobot
                         System.Drawing.Image img = httpHelper.GetHttpResponseImage(verImageUrl);
                         image = this.BitmapToBitmapImage(new System.Drawing.Bitmap(img));
                         this.verifysession = httpHelper.handler.CookieContainer.GetCookies(new Uri(verImageUrl))["verifysession"].ToString().Split('=')[1];
-                        this.qquin = mc[2].Value;
                         break;
                     default:
                         break;
@@ -69,6 +68,11 @@ namespace IMRobot
             return image != null;
         }
 
+        /// <summary>
+        /// 图片转化
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
         public BitmapImage BitmapToBitmapImage(System.Drawing.Bitmap bitmap)
         {
             System.Drawing.Bitmap bitmapSource = new System.Drawing.Bitmap(bitmap.Width, bitmap.Height);
@@ -93,18 +97,35 @@ namespace IMRobot
             return bitmapImage;
         }
 
-        public bool Login(string pwd, string verifiCode, string account)
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="pwd"></param>
+        /// <param name="verifiCode"></param>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public string Login(string pwd, string verifiCode, string account)
         {
+            string loginStr = "登录失败：";
             ///获取密文
             string salt = this.webBrowser.Document.InvokeScript("GetSalt", new object[] { account }).ToString();
             verifiCode = string.IsNullOrEmpty(verifiCode) ? this.codeString : verifiCode;
-            string p = this.webBrowser.Document.InvokeScript("GetEncryption", new object[] { pwd, this.qquin, verifiCode }).ToString();
+            string p = this.webBrowser.Document.InvokeScript("GetEncryption", new object[] { pwd, salt, verifiCode }).ToString();
             string verAccountUrl = string.Format("https://ssl.ptlogin2.qq.com/login?u={0}&verifycode={1}&pt_vcode_v1=0&pt_verifysession_v1={2}&p={3}&pt_randsalt=0&u1=https%3A%2F%2Fmail.qq.com%2Fcgi-bin%2Flogin%3Fvt%3Dpassport%26vm%3Dwpt%26ft%3Dloginpage%26target%3D%26account%3D2949575&ptredirect=1&h=1&t=1&g=1&from_ui=1&ptlang=2052&action=11-23-1449812239662&js_ver=10141&js_type=1&login_sig=JY6zkbxK92OUp5F5mgRGZqWv1hlabR9B4pNFVX6OSrmPv3xnMBjc3SXb1tn7QxwP&pt_uistyle=25&aid=522005705&daid=4&", account, verifiCode, verifysession, p);
             string result = httpHelper.GetHttpResponseString(verAccountUrl);
             Regex regex = new Regex(@"(?<=')[^,]*?(?=')");
             MatchCollection mc = regex.Matches(result);
+            if (mc[0].Value == "0"&&mc[3].Equals("1"))
+            {
+                loginStr = string.Format("登录成功,欢迎您{0}",mc[5].Value);
+                logSuccessUrl = mc[3].Value;
+            }
+            else
+            {
+                loginStr = mc[2].Value;
+            }
 
-            return true;
+            return loginStr;         
         }
     }
 }
